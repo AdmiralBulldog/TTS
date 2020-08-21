aa6e3940690ad13f14e48e9962a16ce44da18d95
Roope Kausiala <roope.kausiala@aalto.fi>
2020-08-21 12:07:11 +0300

    preprocessor for kimi dataset (same as ljspeech)

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -138,6 +138,18 @@ def ljspeech(root_path, meta_file):
     return items
 
 
+def kimi(root_path, meta_file):
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "kimi"
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split('|')
+            wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
+            text = cols[1]
+            items.append([text, wav_file, speaker_name])
+    return items
+
 def nancy(root_path, meta_file):
     """Normalizes the Nancy meta data file to TTS format"""
     txt_file = os.path.join(root_path, meta_file)
